dd8feacf7a19cb5e215ae0634c7e18c027076dd1
lservo <l.servo@hotmail.com>
2022-07-22 21:13:19 -0300

    chore -> reiniciando projeto lets_dev

---
+++ src/database/repositories/UsersRepository.ts
@@ -0,0 +1,35 @@
+import { prisma } from "../../../prisma/PrismaClient"
+import { UserEntity } from "../entities/UserEntity"
+
+type CreateUserDTO ={
+    userData: UserEntity;
+}
+
+type FindByEmailDTO ={
+    email: string;
+}
+
+class UserRepository{
+    async create({ userData }: CreateUserDTO){
+        const newUser = await prisma.user.create({
+            data:{
+                ... userData
+            }
+        });
+
+        return newUser;
+    }
+
+    async findByEmail({ email }: FindByEmailDTO ): Promise<UserEntity>{
+        const userFound = await prisma.user.findFirst({
+            where:{
+                email
+            }
+        });
+
+        return userFound;
+    }
+
+}
+
+export { UserRepository }
